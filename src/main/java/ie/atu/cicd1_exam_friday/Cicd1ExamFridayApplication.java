c88a0ff289616c937033d040e847b9ef2223ef51
MATTHEW MURPHY <g00430349@atu.ie>
2025-11-21 09:07:46 +0000

    Setup exam

---
+++ src/main/java/ie/atu/cicd1_exam_friday/Cicd1ExamFridayApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1_exam_friday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1ExamFridayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1ExamFridayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd1_exam_friday/Cicd1ExamFridayApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1_exam_friday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1ExamFridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
